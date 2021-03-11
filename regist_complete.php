9bbbf2e2978311c7ace766d0e310b54f3a465927
test-saitomini <tennis.lp.710@gmail.com>
2021-03-11 22:22:22 +0900

    20210311 test

--- regist_complete.php
+++ regist_complete.php
@@ -7,7 +7,7 @@ $update_time = $date;
 
 $pdo = new PDO("mysql:dbname=lesson01;host=localhost;","root","");
 
-$pdo -> exec("insert into account(family_name,last_name,family_name_kana,last_name_kana,mail,password,gender,postal_code,prefecture,address_1,address_2,authority,delete_flag,registered_time,update_time)values('".$_POST['family_name']."','".$_POST['last_name']."','".$_POST['family_name_kana']."','".$_POST['last_name_kana']."','".$_POST['mail']."','".$_POST['password']."','".$_POST['gender']."','".$_POST['postal_code']."','".$_POST['prefecture']."','".$_POST['address_1']."','".$_POST['address_2']."','".$_POST['authority']."','".$_POST['delete_flag']."','".date('Y-m-d H:i:s',$registered_time)."','".date('Y-m-d H:i:s',$update_time)."');");
+$pdo -> exec("insert into account(family_name,last_name,family_name_kana,last_name_kana,mail,password,gender,postal_code,prefecture,address_1,address_2,authority,delete_flag,registered_time,update_time)values('".$_POST['family_name']."','".$_POST['last_name']."','".$_POST['family_name_kana']."','".$_POST['last_name_kana']."','".$_POST['mail']."','".password_hash($_POST['password'],PASSWORD_DEFAULT)."','".$_POST['gender']."','".$_POST['postal_code']."','".$_POST['prefecture']."','".$_POST['address_1']."','".$_POST['address_2']."','".$_POST['authority']."','".$_POST['delete_flag']."','".date('Y-m-d H:i:s',$registered_time)."','".date('Y-m-d H:i:s',$update_time)."');");
 ?>
 
 <!DOCTYPE HTML>
