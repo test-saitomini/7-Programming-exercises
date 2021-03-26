e015dbfd0c2bc86fa6a0ce6df63b17a2980b295f
test-saitomini <tennis.lp.710@gmail.com>
2021-03-26 22:06:50 +0900

    20210326 test

--- regist.js
+++ regist.js
@@ -32,6 +32,7 @@ function input_check(){
 	$("#mail_error").empty();
 	$("#password_error").empty();
 	$("#postal_code_error").empty();
+    $("#prefecture_error").empty();
 	$("#address_1_error").empty();
     $("#address_2_error").empty();
 
@@ -43,51 +44,137 @@ function input_check(){
 	var mail = $("#mail").val();
 	var password  = $("#password").val();
 	var postal_code  = $("#postal_code").val();
+    var prefecture  = $("#prefecture").val();
 	var raddress_1  = $("#address_1").val();
     var raddress_2  = $("#address_2").val();
-    
-    alert(family_name);
     
 
 	// 入力内容チェック
 	// お名前
 	if(family_name == ""){
-		$("#family_name_error").html(" お名前は必須です。");
+		$("#family_name_error").html(" 名前（姓）が未入力です。");
 		$("#family_name").addClass("inp_error");
 		result = false;
-	}else if(name.length >10){
-		$("#family_name_error").html(" お名前は10文字以内で入力してください。");
+	}else if(family_name.length >10){
+		$("#family_name_error").html(" 名前（姓）は10文字以内で入力してください。");
 		$("#family_name").addClass("inp_error");
+		result = false;
+	}else if(!family_name.match(/^[\u3040-\u309f\u30e0-\u9fcf]+$/)){
+        $("#family_name_error").html(" 名前（姓）は漢字かひらがなで入力してください。");
+		$("#family_name").addClass("inp_error");
+		result = false;
+    }
+    /*if(last_name == ""){
+		$("#last_name_error").html(" 名前（名）が未入力です。。");
+		$("#last_name").addClass("inp_error");
+		result = false;
+	}else if(last_name.length >10){
+		$("#last_name_error").html(" 名前（名）は10文字以内で入力してください。");
+		$("#last_name").addClass("inp_error");
+		result = false;
+	}/*else if(!last_name.match(/^[\u3040-\u309f\u30e0-\u9fcf]+$/)){
+        $("#last_name_error").html(" 名前（名）は漢字かひらがなで入力してください。");
+		$("#last_name").addClass("inp_error");
+		result = false;
+    }
+	// フリガナ
+	if(family_name_kana == ""){
+		$("#family_name_kana_error").html(" カナ（姓）が未入力です。");
+		$("#family_name_kana").addClass("inp_error");
+		result = false;
+	}else if(!family_name_kana.match(/^[\u30a0-\u30ff]+$/)){
+		$("#family_name_kana_error").html(" カナ（姓）は全角カタカナで入力してください。");
+		$("#family_name_kana").addClass("inp_error");
+		result = false;
+	}else if(family_name_kana.length > 10){
+		$("#family_name_kana_error").html(" カナ（姓）は10文字以内で入力してください。");
+		$("#family_name_kana").addClass("inp_error");
 		result = false;
 	}
-	/*// フリガナ
-	if(furigana == ""){
-		$("#furigana_error").html(" フリガナは必須です。");
-		$("#furigana").addClass("inp_error");
+    if(last_name_kana == ""){
+		$("#last_name_kana_error").html(" カナ（名）が未入力です。");
+		$("#last_name_kana").addClass("inp_error");
 		result = false;
-	}else if(!furigana.match(/^[ァ-ロワヲンー 　\r\n\t]*$/)){
-		$("#furigana_error").html(" フリガナは全角カタカナで入力してください。");
-		$("#furigana").addClass("inp_error");
+	}else if(!last_name_kana.match(/^[\u30a0-\u30ff]+$/)){
+		$("#last_name_kana_error").html(" カナ（名）は全角カタカナで入力してください。");
+		$("#last_name_kana").addClass("inp_error");
 		result = false;
-	}else if(furigana.length > 25){
-		$("#furigana_error").html(" フリガナは25文字以内入力してください。");
-		$("#furigana").addClass("inp_error");
+	}else if(last_name_kana.length > 10){
+		$("#last_name_kana_error").html(" カナ（名）は10文字以内で入力してください。");
+		$("#last_name_kana").addClass("inp_error");
 		result = false;
 	}
 	// メールアドレス
-	if(mailaddress == ""){
-		$("#mailaddress_error").html(" メールアドレスは必須です。");
-		$("#mailaddress").addClass("inp_error");
+	if(mail == ""){
+		$("#mail_error").html(" メールアドレスが未入力です。");
+		$("#mail").addClass("inp_error");
 		result = false;
-	}else if(!mailaddress.match(/^([a-zA-Z0-9])+([a-zA-Z0-9\._-])*@([a-zA-Z0-9_-])+([a-zA-Z0-9\._-]+)+$/)){
-		$('#mailaddress_error').html(" 正しいメールアドレスを入力してください。");
-		$("#mailaddress").addClass("inp_error");
+	}else if(!mail.match(/^([a-zA-Z0-9])+([a-zA-Z0-9\._-])*@([a-zA-Z0-9_-])+([a-zA-Z0-9\._-]+)+$/)){
+		$('#mail_error').html(" 正しいメールアドレスを入力してください。");
+		$("#mail").addClass("inp_error");
 		result = false;
-	}else if(mailaddress.length > 255){
-		$('#mailaddress_error').html(" 正しいメールアドレスを入力してください。");
-		$("#mailaddress").addClass("inp_error");
+	}else if(mail.length > 100){
+		$('#mail_error').html(" メールアドレスは100字以内で入力してください。");
+		$("#mail").addClass("inp_error");
 		result = false;
-	}*/
-    alert(result);
+	}
+    //パスワード
+    if(password == ""){
+		$("#password_error").html(" パスワードが未入力です。");
+		$("#password").addClass("inp_error");
+		result = false;
+	}else if(password.length >10){
+		$("#password_error").html(" パスワードは10文字以内で入力してください。");
+		$("#password").addClass("inp_error");
+		result = false;
+	}/*else if(!password.match(/^[0-9a-zA-Z]+$/)){
+        $("#password_error").html(" パスワードは10文字以内で入力してください。");
+		$("#password").addClass("inp_error");
+		result = false;
+    }
+    //郵便番号
+    if(postal_code == ""){
+		$("#postal_code_error").html(" 郵便番号が未入力です。");
+		$("#postal_code").addClass("inp_error");
+		result = false;
+	}else if(postal_code.length >7){
+		$("#postal_code_error").html(" 郵便番号は7文字以内で入力してください。");
+		$("#postal_code").addClass("inp_error");
+		result = false;
+	}else if(!postal_code.match(/^[0-9]+$/)){
+        $("#postal_code_error").html(" 郵便番号は半角数字のみ入力してください。");
+		$("#postal_code").addClass("inp_error");
+		result = false;
+    }
+    //住所（都道府県）
+    if(prefecture == ""){
+		$("#prefecture_error").html(" 住所（都道府県）が未選択です。");
+		$("#prefecture").addClass("inp_error");
+		result = false;
+	}
+    //住所（市区町村）
+    if(address_1 == ""){
+		$("#faddress_1_error").html(" 住所（市区町村）が未入力です。");
+		$("#address_1").addClass("inp_error");
+		result = false;
+	}else if(last_name.length >10){
+		$("#address_1_error").html(" 住所（市区町村）は10文字以内で入力してください。");
+		$("#address_1").addClass("inp_error");
+		result = false;
+	}//else if(!address_1.match()){
+    
+    //}
+    //住所（番地）
+    if(address_2 == ""){
+		$("#address_2_error").html(" 住所（番地）が未入力です。");
+		$("#address_2").addClass("inp_error");
+		result = false;
+	}else if(address_2.length >100){
+		$("#address_2_error").html(" 住所（番地）は100文字以内で入力してください。");
+		$("#address_2").addClass("inp_error");
+		result = false;
+	}//else if(!password.match()){
+    
+    //}*/
     return result;
 }
