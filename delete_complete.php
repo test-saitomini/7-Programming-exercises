9ff551dbf8f28d8f314896706c961c9672b452fd
test-saitomini <79200403+test-saitomini@users.noreply.github.com>
2021-07-16 21:51:06 +0900

    20210716 test

--- delete_complete.php
+++ delete_complete.php
@@ -3,9 +3,30 @@ mb_language('ja');
 mb_internal_encoding("UTF-8");
 
 $id = $_POST['id'];
+$delete_flag = $_POST['delete_flag'];
 
 $pdo = new PDO("mysql:dbname=lesson01;host=localhost;","root","");
 
-$stmt = $dbh->prepare('UPDATE account SET delete_flag = $delete_flag where id = '.$id);
+$stmt = $pdo->prepare("UPDATE account SET delete_flag =".$delete_flag." where id = ".$id);
 
-$stmt->execute(array('$delete_flag' => $_POST['delete_flag']));
+//$stmt->execute(array('$delete_flag' => $_POST['delete_flag']));
+
+?>
+
+<!DOCTYPE HTML>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <title>アカウント削除完了画面</title>
+        <link rel="stylesheet"type="text/css" href="regist.css">
+    </head>
+    <body>
+        <div class="back-top">
+            <h4>削除完了しました。</h4>
+             <form action="regist.html" method="post">
+            <input type="submit" class="button2" value="TOPページに戻る">
+            </form>
+        </div>
+    </body>
+    
+</html>
