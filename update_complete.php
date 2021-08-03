98a33c12a0701e0a3acb558b2c0b7bdae88b1e48
test-saitomini <tennis.lp.710@gmail.com>
2021-08-03 22:23:22 +0900

    20210803 test

--- update_complete.php
+++ update_complete.php
@@ -1,6 +1,7 @@
 <?php
 mb_language('ja');
 mb_internal_encoding("UTF-8");
+date_default_timezone_set('Asia/Tokyo');
 
 $id = $_POST['id'];
 $family_name = $_POST['family_name'];
@@ -15,25 +16,17 @@ $prefecture = $_POST['prefecture'];
 $address_1 = $_POST['address_1'];
 $address_2 = $_POST['address_2'];
 $authority = $_POST['authority'];
-$delete_flag = $_POST['delete_flag']; 
+$delete_flag = $_POST['delete_flag'];
+
+
 
 $pdo = new PDO("mysql:dbname=lesson01;host=localhost;","root","");
 
 $stmt = $pdo->prepare("UPDATE account SET family_name = $family_name,last_name = $last_name,family_name_kana = $family_name_kana,last_name_kana =  $last_name_kana,mail = $mail,password = $password,gender = $gender,postal_code = $postal_code,prefecture = $prefecture,address_1 = $address_1,address_2 = $address_2,authority = $authority,delete_flag = $delete_flag where id = $id");
 
-$stmt->execute(array('$family_name' => $_POST['family_name'],
-                     '$last_name' => $_POST['last_name'],
-                     '$family_name_kana' => $_POST['family_name_kana'],
-                     '$last_name_kana' => $_POST['last_name_kana'],
-                     '$mail' => $_POST['mail'],
-                     '$password' => password_hash($_POST['password'],PASSWORD_ARGON2ID),
-                     '$gender' => $_POST['gender'],
-                     '$postal_code' => $_POST['postal_code'],
-                     '$prefecture' => $_POST['prefecture'],
-                     '$address_1' => $_POST['address_1'],
-                     '$address_2' => $_POST['address_2'],
-                     '$authority' => $_POST['authority'],
-                     '$delete_flag' => $_POST['delete_flag']));
+$registered_time = $_POST['registered_time'];
+
+$stmt ->exec("family_name,last_name,family_name_kana,last_name_kana,mail,password,gender,postal_code,prefecture,address_1,address_2,authority,delete_flag,registered_time,update_time)values('".$family_name."','".$last_name."','".$family_name_kana."','".$last_name_kana."','".$mail."','".$password."','".$gender."','".$postal_code."','".$prefecture."','".$address_1."','".$address_2."','".$authority."','".$delete_flag."','".$registered_time."','".date('Y-m-d H:i:s')."');");
 
 ?>
 
