21de5778de5749a47ae42b803412bf83b2055bbb
test-saitomini <tennis.lp.710@gmail.com>
2021-10-18 22:10:42 +0900

    20211018 test

--- list.php
+++ list.php
@@ -71,6 +71,7 @@ if($_SESSION != NULL){
                             <td class="list_center">
                                 <label for="0"><input id="0" type="radio" name="gender"value="0" checked>男</label>
                                 <label for="1"><input id="1" type="radio" name="gender"value="1">女</label>
+                                <label for="1"><input id="2" type="radio" name="gender"value="2">選択なし</label>
                            </td>
                        </tr>
                        <tr>
@@ -78,15 +79,38 @@ if($_SESSION != NULL){
                             <td class="list_center"><select class="dropdown" name="authority">
                                 <option value='0'　checked>一般</option>
                                 <option value='1'>管理者</option>
+                                <option value='2'>選択なし</option>
                                 </select></td>
                        </tr>
                        <tr>
-                           <td class ="center"><input type="submit" name="kensaku"  id="btn_confirm"value="検索"></td>
+                           <td class ="center"><input type="submit" name="kensaku"  id="kensaku"value="検索"></td>
                        </tr>
                        
                     </table>
                 </div>
             </form>
+            
+            <?php
+            
+            if (isset($_POST['kensaku'])) {
+            
+                mb_internal_encoding("UTF-8");
+                $pdo = new PDO("mysql:dbname=lesson01;host=localhost;","root","");
+            
+                if($_POST['family_name'] != " " || $_POST['last_name'] != " " 
+                   || $_POST['family_name_kana'] != " " || $_POST['last_name_kana'] != " " 
+                   || $_POST['mail'] != " " || $_POST["gender"] != "2" || $_POST["authority"] != "2"){
+                    $stmt = $pdo -> query("select * from account where family_name LIKE '".$_POST['family_name']."' OR last_name LIKE '".$_POST["last_name"]."' OR family_name_kana LIKE '".$_POST["family_name_kana"]."' OR last_name_kana LIKE '".$_POST["last_name_kana"]."' OR mail LIKE '".$_POST["mail"]."' OR gender='".$_POST["gender"]."' OR authority='".$_POST['authority']."' ORDER BY id DESC");
+                    
+                    
+                }else{
+                $stmt = $pdo -> query("select * from account ORDER BY id DESC");
+                    
+                    
+                }
+            }
+            
+            ?>
             
             <div class = "kakunin_area">
             <table class="accounttable">
@@ -105,120 +129,117 @@ if($_SESSION != NULL){
                     <td class="center" colspan="2">操作</td>
                 </tr>
             <?php
-            
-            mb_internal_encoding("UTF-8");
-            $pdo = new PDO("mysql:dbname=lesson01;host=localhost;","root","");
-            
-            $stmt = $pdo -> query("select * from account ORDER BY id DESC");           
-            while($row = $stmt->fetch()){
-                echo '<tr>';
-                echo '<td class="right">',$row['id'],'</td>';
-                echo '<td class="right">',$row['family_name'],'</td>';
-                echo '<td class="right">',$row['last_name'],'</td>';
-                echo '<td class="right">',$row['family_name_kana'],'</td>';
-                echo '<td class="right">',$row['last_name_kana'],'</td>';
-                echo '<td class="left">',$row['mail'],'</td>';
-                if($row['gender'] === "0"){
-                    echo '<td class="center">',"男",'</td>';
-                } else {
-                    echo '<td class="center">',"女",'</td>';
+                if (isset($_POST['kensaku'])) {
+                    while($row = $stmt->fetch()){
+                        echo '<tr>';
+                        echo '<td class="right">',$row['id'],'</td>';
+                        echo '<td class="right">',$row['family_name'],'</td>';
+                        echo '<td class="right">',$row['last_name'],'</td>';
+                        echo '<td class="right">',$row['family_name_kana'],'</td>';
+                        echo '<td class="right">',$row['last_name_kana'],'</td>';
+                        echo '<td class="left">',$row['mail'],'</td>';
+                        if($row['gender'] === "0"){
+                            echo '<td class="center">',"男",'</td>';
+                        } else {
+                            echo '<td class="center">',"女",'</td>';
+                        }
+                        if($row['authority'] === "0"){
+                            echo '<td class="center">',"一般",'</td>';
+                        } else {
+                            echo '<td class="center">',"管理者",'</td>';
+                        }
+                        if($row['delete_flag'] === "0"){
+                            echo '<td class="center">',"有効",'</td>';
+                        } else {
+                            echo '<td class="center">',"無効",'</td>';
+                        }
+
+                        $date1 = $row['registered_time'];
+                        $registered_date = substr($date1,0,10);
+                        echo '<td class="center">',$registered_date,'</td>';
+
+                        $date2 = $row['update_time'];
+                        $update_date = substr($date2,0,10);
+                        echo '<td class="center">',$update_date,'</td>';
+
+                        //各行の表に対応した更新ボタン
+                        echo '<form action="update.php" method="post">';
+                        echo '<input type = "hidden" name = "id" value="'.$row["id"].'">';
+                        if($row['delete_flag']== 1){
+                            echo '<td class="center">','<input type="submit" value="更新" disabled>
+                    </form>','</td>';
+                        }else{
+                            echo '<td class="center">','<input type="submit" value="更新">
+                    </form>','</td>';
+                        };
+
+
+                        //各行の表に対応した削除ボタン
+                        echo '<form action="delete.php" method="post">';
+                        echo '<input type = "hidden" name = "id" value="'.$row["id"].'" >';
+                        //echo "<input type = hidden value=".$row["id"].">";
+                        if($row['delete_flag']== 1){
+                            echo '<td class="center">','<input type="submit" value="削除"disabled>
+                    </form>','</td>';
+                        }else{
+                            echo '<td class="center">','<input type="submit" value="削除">
+                    </form>','</td>';
+                        }
+
+
+                        echo '</tr>';
+                    }
                 }
-                if($row['authority'] === "0"){
-                    echo '<td class="center">',"一般",'</td>';
-                } else {
-                    echo '<td class="center">',"管理者",'</td>';
-                }
-                if($row['delete_flag'] === "0"){
-                    echo '<td class="center">',"有効",'</td>';
-                } else {
-                    echo '<td class="center">',"無効",'</td>';
-                }
-                
-                $date1 = $row['registered_time'];
-                $registered_date = substr($date1,0,10);
-                echo '<td class="center">',$registered_date,'</td>';
-                
-                $date2 = $row['update_time'];
-                $update_date = substr($date2,0,10);
-                echo '<td class="center">',$update_date,'</td>';
-                
-                //各行の表に対応した更新ボタン
-                echo '<form action="update.php" method="post">';
-                echo '<input type = "hidden" name = "id" value="'.$row["id"].'">';
-                if($row['delete_flag']== 1){
-                    echo '<td class="center">','<input type="submit" value="更新" disabled>
-            </form>','</td>';
-                }else{
-                    echo '<td class="center">','<input type="submit" value="更新">
-            </form>','</td>';
-                };
-                
-                
-                //各行の表に対応した削除ボタン
-                echo '<form action="delete.php" method="post">';
-                echo '<input type = "hidden" name = "id" value="'.$row["id"].'" >';
-                //echo "<input type = hidden value=".$row["id"].">";
-                if($row['delete_flag']== 1){
-                    echo '<td class="center">','<input type="submit" value="削除"disabled>
-            </form>','</td>';
-                }else{
-                    echo '<td class="center">','<input type="submit" value="削除">
-            </form>','</td>';
-                }
-                
-                
-                echo '</tr>';
-            }
-            ?>
-                
-            
-            </table>
+                ?>
+
+
+                </table>
+                    </div>
+
+            </main>
+            <?php elseif($login_authority == 0) : ?>
+            <header>
+                <ul>
+                    <li>トップ</li>
+                    <li>プロフィール</li>
+                    <li>アカウント登録</li>
+                    <li>問い合わせ</li>
+                    <li>アカウント一覧</li>
+                    <li>その他</li>
+                </ul>
+            </header>
+            <main>
+                <div class="error_messge">
+                    <h8>※この画面は操作できません。</h8>
+                    <form action="login.php" >
+                        <input type="submit" class="submit" value="ログイン画面へ戻る">
+                    </form>
                 </div>
-            
-        </main>
-        <?php elseif($login_authority == 0) : ?>
-        <header>
-            <ul>
-                <li>トップ</li>
-                <li>プロフィール</li>
-                <li>アカウント登録</li>
-                <li>問い合わせ</li>
-                <li>アカウント一覧</li>
-                <li>その他</li>
-            </ul>
-        </header>
-        <main>
-            <div class="error_messge">
-                <h8>※この画面は操作できません。</h8>
-                <form action="login.php" >
-                    <input type="submit" class="submit" value="ログイン画面へ戻る">
-                </form>
-            </div>
-        </main>
-        <?php else : ?>
-        <header>
-            <ul>
-                <li>トップ</li>
-                <li>プロフィール</li>
-                <li>アカウント登録</li>
-                <li>問い合わせ</li>
-                <li>アカウント一覧</li>
-                <li>その他</li>
-            </ul>
-        </header>
-        <main>
-            <div class="error_messge">
-                <h8>※ログインを行ってください。</h8>
-                <form action="login.php">
-                    <input type="submit" class="submit" value="ログイン画面へ戻る">
-                </form>
-            </div>
-        </main>
-        <?php endif; ?>
-        
-        <footer>
-            Copyright D.I.Works| D.I.blog is the one which provides Ato Z about programming
-        </footer>
-        <script type="text/javascript" src="regist.js"></script>
-    </body>
-</html>
+            </main>
+            <?php else : ?>
+            <header>
+                <ul>
+                    <li>トップ</li>
+                    <li>プロフィール</li>
+                    <li>アカウント登録</li>
+                    <li>問い合わせ</li>
+                    <li>アカウント一覧</li>
+                    <li>その他</li>
+                </ul>
+            </header>
+            <main>
+                <div class="error_messge">
+                    <h8>※ログインを行ってください。</h8>
+                    <form action="login.php">
+                        <input type="submit" class="submit" value="ログイン画面へ戻る">
+                    </form>
+                </div>
+            </main>
+            <?php endif; ?>
+
+            <footer>
+                Copyright D.I.Works| D.I.blog is the one which provides Ato Z about programming
+            </footer>
+            <script type="text/javascript" src="regist.js"></script>
+        </body>
+    </html>
